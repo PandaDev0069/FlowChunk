75b7de40450a61ac4e220d168bbe3617c06c9432
PandaDev0069 <ashispanta100@gmail.com>
2026-06-17 17:01:54 +0900

    refactor: move creation of test_user to separate function

--- tests/services/test_user_service.py
+++ tests/services/test_user_service.py
@@ -9,17 +9,23 @@ from app.services import user_service
 
 
 @pytest.fixture
-def test_user():
-    return UserCreate(
+def test_user(db_session) -> UserOrm:
+    test_user = UserCreate(
         username="testuser",
         email="test@example.com",
         password="securepassword",
     )
-
-
-def test_register_user_success(db_session, test_user):
-    test_user = test_user
     user = user_service.register_user(test_user, db_session)
+    return user
+
+
+def test_register_user_success(db_session):
+    user_in = UserCreate(
+        username="testuser",
+        email="test@example.com",
+        password="securepassword",
+    )
+    user = user_service.register_user(user_in, db_session)
     assert isinstance(user, UserOrm)
     assert user.id is not None
     assert user.username == "testuser"
@@ -63,9 +69,8 @@ def test_register_user_duplicate_email(db_session):
 
 
 def test_get_user_or_404_returns_user(db_session, test_user):
-    user = user_service.register_user(test_user, db_session)
-    retrieved_user = user_service.get_user_or_404(user_id=user.id, db=db_session)
-    assert retrieved_user == user
+    retrieved_user = user_service.get_user_or_404(user_id=test_user.id, db=db_session)
+    assert retrieved_user == test_user
 
 
 def test_get_user_or_404_not_found(db_session):
@@ -77,11 +82,10 @@ def test_get_user_or_404_not_found(db_session):
 
 
 def test_get_user_or_404_deleted_user(db_session, test_user):
-    user = user_service.register_user(test_user, db_session)
-    user.is_deleted = True
+    test_user.is_deleted = True
     db_session.commit()
 
     with pytest.raises(HTTPException) as exc_info:
-        user_service.get_user_or_404(user_id=user.id, db=db_session)
+        user_service.get_user_or_404(user_id=test_user.id, db=db_session)
     assert exc_info.value.status_code == status.HTTP_404_NOT_FOUND
     assert exc_info.value.detail == "User not found"
