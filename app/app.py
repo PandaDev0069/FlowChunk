f6ae304b4f78437809609532fc3ad01edd43cb2a
PandaDev0069 <ashispanta100@gmail.com>
2026-06-24 13:57:53 +0900

    feat: allow all cors origin

--- app/app.py
+++ app/app.py
@@ -1,11 +1,20 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.database import SessionLocal
 from app.routers import admin, user
 from app.tasks.cleanup import run_user_cleanup
 
 app = FastAPI(title="FlowChunk API", version="0.0.1")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router)
 app.include_router(admin.router)
@@ -23,7 +32,6 @@ scheduler = BackgroundScheduler()
 
 def cleanup_job():
     db = SessionLocal()
-
     try:
         run_user_cleanup(db)
     finally:
